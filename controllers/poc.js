e2d575b10c344dc23a601730408c59470028ee31
neung <neungszad@gmail.com>
2016-10-24 19:29:55 +0700

    initial project calculate depth draft version

---
+++ controllers/poc.js
@@ -0,0 +1,230 @@
+"use strict";
+
+const fs = require('fs');
+const sql = require('mssql');
+const stopWordEnglish = require('../stop-word-english').english;
+const sw = require('stopword');
+const _ = require('underscore');
+const tree = require('../data/tree.js');
+const config = {
+    user: 'sa',
+    password: '1234',
+    server: 'localhost',
+    database: 'StackOverflow',
+    requestTimeout: 60000, 
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    }
+};
+
+var visitNode = {};
+
+/**
+ * GET /
+ * Home page.
+ */
+exports.index = (req, res) => {
+  const difficuty = ["Normal", "Normal", "Easy", "Easy", "Easy", "Hard", "Normal", "Hard", "Easy", "Hard",
+                     "Normal", "Normal", "Easy", "Easy", "Easy", "Normal", "Hard", "Easy", "Easy", "Hard"];
+  fs.readFile('./data/questions2.json', (err, data) => {
+    const result = JSON.parse(data);
+    const kwTitle = {};
+    const tags = {};
+    const bodyContent = {};
+    const bodyCode = {};
+    const bodyFull = {};
+    const scope = {};
+    const finalScope = {};
+
+    result.map(q => {
+      //visitNode = {};
+
+      // title
+      const oldString = q.Title.split(' ');
+      kwTitle[q.Id] = sw.removeStopwords(uniqueList(removeSpecialChar(oldString)), stopWordEnglish);
+      scope[q.Id] = calScore(kwTitle[q.Id], false)*2;
+
+      // tags
+      tags[q.Id] = uniqueList(removeSpecialChar(extractTags(q.Tags)));
+      scope[q.Id] += calScore(tags[q.Id], false)*2;
+
+      // body 
+      const tempBody = retriveBodyText(q.Body);
+      bodyContent[q.Id] = sw.removeStopwords(uniqueList(tempBody.content), stopWordEnglish);
+      bodyCode[q.Id] = sw.removeStopwords(uniqueList(tempBody.code), stopWordEnglish);
+      scope[q.Id] += calScore(bodyContent[q.Id], false);
+      scope[q.Id] += calScore(bodyCode[q.Id], true);
+
+      // body full
+      bodyFull[q.Id] = q.Body;
+      
+      finalScope[q.Id] = calScopeScore(scope[q.Id]);
+
+      //console.log(visitNode);
+    });
+
+    res.render('poc', {
+      title: 'PoC',
+      rows: result,
+      kwTitle,
+      tags,
+      bodyContent,
+      bodyCode,
+      scope,
+      finalScope,
+      difficuty,
+      bodyFull
+    });
+  });
+};
+
+const extractTags = (tags) => {
+  var temp = tags.slice(1, -1);
+  return temp.split('><');
+}
+
+const removeSpecialChar = (words) => {
+  return words.map(w => {
+    return w.replace(/[`~@?;:,"“”{}\(\);,']/gi, '').toLowerCase();
+    // replace(/[^\w\s\-\.]/gi, '')
+    // replace(/[`~!@?;:,<>\{\}\[\]\\\/]/gi, '')
+  })
+}
+
+const uniqueList = (words) => {
+  const result = _.without(words, 'javascript')
+  return _.uniq(result);
+}
+
+const calScore = (words, isCode) => {
+  var sum = 0;
+  words.map(w => {
+    // already visited
+    /*if(visitNode[w]) 
+      return 0;*/
+
+    // for debug
+    if(tree[w])
+      console.log("WORD = "+tree[w].word);
+
+    sum += tree[w] ? recusiveCal(tree[w], isCode) : 0 ;  
+  });
+
+  return sum;
+}
+
+const recusiveCal = (word, isCode) => { 
+  //visitNode[word.word] = true;
+
+  // check isCode
+  if(word.isCode && !isCode)
+    return 0;
+
+  if(word.parent){
+    console.log(">> word = " + word.word + " , parent = "+word.parent);
+    return word.value + recusiveCal(tree[word.parent], isCode);
+  }
+
+  return word.value
+}
+
+function calScopeScore(score) {
+  return 1 - ( 1 / Math.pow(2.71828, score) );
+}
+
+function retriveBodyText(html) {
+  //const html = "<p>I'm having problems getting some setup code executed when my extension is installed. I'm using chrome.runtime.onInstalled as suggested by Chrome Developers' page, but it is not firing. It seems that the issue is related with the use of Require.JS. This is my code:</p>\n\n<pre><code>requirejs.config({...});// i'll not reproduce the whole config here for brevity\n\n// Define the Global Object that will hold the extension functionality\nvar EXT = EXT || {};\n\n// Start the main app logic.\n\nrequirejs(['chrome.storage', 'chrome.settings', 'chrome.tabs'],\nfunction (chromeStorage, chromeSettings, chromeTabs) {\n    'use strict';\n\n    var getLocalRepos;\n\n    /**\n     * Executes the callback with an array of repositories as the parameter.\n     * @param {function} callback\n     */\n    getLocalRepos = function (callback) {\n        'use strict';\n\n        chromeStorage.get('localRepos', callback);\n    };\n\n    // Take care of the extension lifecycle.\n    chrome.runtime.onInstalled.addListener(function (details) {\n        \"use strict\";\n\n        // Create dummy data to populate the local storage for testing purposes.\n        var dummyData = [\n            {\n                name: 'repo1',\n                description: 'description1',\n                username: 'reydel',\n                age: 'theAge'\n            },\n            {\n                name: 'repo2',\n                description: 'description2',\n                username: 'reydel',\n                age: 'theAge'\n            }\n        ];\n\n        switch (details.reason) {\n            case 'install':\n                chromeStorage.set({ localRepos: dummyData }, function () {\n                    // Do nothing\n                });\n                break;\n        }\n    });\n\n    // Bind all functions to an object in the Global Space to make them accessible from the outside scripts\n    // referencing the BackgroundPage object\n    window.EXT.getLocalRepos = getLocalRepos;\n});\n</code></pre>\n\n<p>I have used the code inside the listener's callback in the console and it works, it's just that the event is not being triggered.</p>\n\n<p>Any ideas on how to solve this? Someone have done it before?</p>\n";
+  //html = html.replace(/\\(?:n|r)/gi, ' ');
+  //const re = /(?:<pre>|<\/pre>)/gi;
+  const re = /(?:<pre\s?(?:.)*?>|<\/pre\s?(?:.)*?>)/gi;
+  const paragraphs = html.split(re);
+
+  let content = "",
+      code = "";
+
+  //console.log(paragraphs);    
+  paragraphs.map(w => {
+    if(w.substring(0,6) === "<code>")
+      code += "" + w;
+    else
+      content += "" + w;
+  });
+
+  // content remove tag 
+  content = content.replace(/<(?:.|\n)*?>/gm, '');
+  // content replace Speaial Chat to space bar
+  content = content.replace(/[`~@?;:,"“”{}();,\.']/gi, ' ');
+  // spit 
+  content = content.toLowerCase().split(/\s+/);
+  /*console.log("content = ");
+  console.log(code);*/
+  // convert entity to tag
+  code = _.unescape(code);
+  // remove stop word for programming
+  code = code.replace(/([@:`\'\"{}\(\)\[\];,'\.<>\/?])/gm, ' ').split(/\s+/);
+
+
+  return {
+    content,
+    code 
+  }
+}
+
+const _getArrayIndex = (start, end) => {
+  let result = [];
+  while(start <= end){
+    result.push(arr[start]);
+    start++;
+  } 
+  return result.join(' ');
+}
+
+const arrMulti3 = (arr) => {
+  const max = arr.length;
+  const result = [];
+  
+  for(let i=1; i<=3; i++){
+    let start = 0;
+    let end;
+    while(start < max){
+       end = start+(i-1);
+       if(end<max){
+         result.push(_getArrayIndex(start, end));
+       }
+       start++;         
+    }    
+  } 
+  
+  return result;
+}
+
+/*
+    const words = ["Javascript","Language","Grammar","Whitespace ","Names ","Numbers ","Strings ","Statements ","Expressions ","Literals ","Variables","Keywords and reserved words","Conditional  ","if","else","do while","while do ","loops","Comments","Operation ","++","--","+-","Functions ","Function Objects ","Function Literal ","Invocation ","Arguments ","Return ","Exceptions ","Augmenting Types ","Recursion ","Scope ","Closure ","Callbacks ","Module ","Cascade ","Curry ","Memoization ","Higher-Order Functions","bind","call","Data Structures","Objects ","Object Literals ","Retrieval ","Update ","Reference ","Prototype ","Reflection ","Enumeration ","Delete ","Global Abatement ","Arrays ","Array Literals ","Length ","Delete ","Enumeration ","Confusion ","Methods ","Dimensions ","Regular Expressions","characters ","pattern ","Grouping subexpressions ","Matches  ","groups ","date type ","Word ","string boundaries ","Backtracking ","search  ","lastIndex ","International ","Inheritance ","Pseudoclassical  ","Object Specifiers  ","Prototypal  ","Functional  ","Parts ","Modules","ES6 ","Variables","Error Handling ","Strict mode ","Testing ","Debugging ","Error propagation ","Exceptions ","Selective catching ","Assertions ","Browser","DOM","Document structure ","Trees ","standard ","Finding elements ","Changing the document ","Creating nodes ","Attributes ","Layout ","Styling ","Cascading styles ","Query selectors ","Positioning and animating ","Handling Events","Event handlers ","Events and DOM nodes ","Event objects ","Propagation ","Default actions ","Key events ","Mouse clicks ","Mouse motion ","Scroll events ","Focus events ","Load event ","Script execution timeline ","Setting timers ","Debouncing","Canvas","SVG","Filling and stroking ","Paths ","Curves ","Text ","Images ","Transformation ","Storing and clearing transformations","HTTP","Promises"];
+
+    words.map(w => {
+      console.log('"'+w.toLowerCase()+'" : {"word":"'+w.toLowerCase()+'","value": 1,"parent":null},');
+    })*/
+
+/* Save to database */ 
+/*exports.index = (req, res) => {
+	var connection1 = new sql.Connection(config, function(err) {	
+    const query = `
+				select top 20 * 
+				from Posts
+        where Tags like \'%javascript%\'
+        and YEAR(CreationDate) = 2015
+    `;
+
+    var request = new sql.Request(connection1);
+    request.query(query, function(err, recordset) {
+      fs.writeFile('../questions.json', JSON.stringify(recordset));
+      console.log("Write Completed.");
+			res.render('poc', {
+			  title: 'PoC',
+			  rows: recordset
+			});
+		});	
+  });
+};*/
