acce423d495c735d861db7d2a7783aeb046432c1
Neung V <neungszad@gmail.com>
2017-03-11 01:49:53 +0700

    Improve Tree

--- src/controllers/poc.js
+++ src/controllers/poc.js
@@ -252,7 +252,7 @@ const calScore = (words, isCode) => {
     if (tree[w]) {
       // check first time visited node 
       if ((tree[w].isCode && isCode) || !tree[w].isCode) {
-        visitNode[tree[w].word] = true;
+        //visitNode[tree[w].word] = true;
         console.log(tree[w].word + " | isCode = " + tree[w].isCode);
       }
 
@@ -274,15 +274,26 @@ const recusiveCal = (word, isCode) => {
   if (word.parent) {
     //console.log(">> word = " + word.word + " , parent = " + word.parent);
 
+    // if visited not sum value in this node 
     let val = word.value;
     if(visitNode[word.word])
       val = 0;
     else
-      visitNode[word.word] = true;
+      visitNode[word.word] = true;  
+
+    console.log('--> ' + word.word + ' = ' + val);
 
     return val + recusiveCal(tree[word.parent], isCode);
   }
 
+  console.log('--> (L) ' + word.word + ' = ' + word.value);  
+
+  // root node is "javascript"
+  if(visitNode[word.word])
+    return 0;
+  else 
+    visitNode[word.word] = true;  
+  
   return word.value;
 }
 
@@ -330,6 +341,15 @@ function retriveBodyText(html) {
  
   // convert entity to tag
   code = _.unescape(code);
+
+  // console.log('---before--');
+  // console.log(code);
+
+  // remove html and php tag and content 
+  code = _clearOtherLangTag(code);
+
+  // console.log('---after--');
+  // console.log(code);
   
   // remove stop word for programming
   code = removeDigi(code.replace(/([@:`\'\"{}\(\)\[\];,'\.<>\/?])/gm, ' ').split(/\s+/));
@@ -338,6 +358,16 @@ function retriveBodyText(html) {
     content,
     code
   }
+}
+
+const _clearOtherLangTag = (code) => {
+  // remove html tag
+  let temp = code.replace(/(<(a|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|big|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hr|html|i|iframe|img|input|ins|kbd|keygen|label|legend|li|link|main|map|mark|menu|menuitem|meta|meter|nav|noframes|noscript|object|ol|optgroup|option|output|p|param|picture|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr)(?:.|\n)*?>(.|\n)*?<\/(a|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|big|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hr|html|i|iframe|img|input|ins|kbd|keygen|label|legend|li|link|main|map|mark|menu|menuitem|meta|meter|nav|noframes|noscript|object|ol|optgroup|option|output|p|param|picture|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr)>)|<\/(a|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|big|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hr|html|i|iframe|img|input|ins|kbd|keygen|label|legend|li|link|main|map|mark|menu|menuitem|meta|meter|nav|noframes|noscript|object|ol|optgroup|option|output|p|param|picture|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr)>/gm, ' ')
+
+  // remove php tag
+  temp = temp.replace(/<\?php(.|\n)*?\?>/gm, ' ')
+
+  return temp;
 }
 
 const _getArrayIndex = (arr, start, end) => {
