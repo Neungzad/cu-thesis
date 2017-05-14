7535573d144a6501416514bf18ddb4ca266c9bfd
Neung V <neungszad@gmail.com>
2017-05-14 15:43:41 +0700

    Refactor filter by tag

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,8 +1,8 @@
 import chalk from 'chalk'
 import _ from 'underscore'
-// import axios from 'axios'
-// import appConfig from '../config/appConfig'
-import resQuestions from '../data/api_so_questions.mini.json'
+import axios from 'axios'
+import appConfig from '../config/appConfig'
+// import resQuestions from '../data/api_so_questions.mini.json'
 import { getDifficultyLevel, connectDB } from './poc'
 var ta = require('../lib/timeAgo.js')()
 
@@ -11,10 +11,14 @@ var ta = require('../lib/timeAgo.js')()
  * Home page.
  */
 export const index = async (req, res) => {
+  const pagesize = 100
+  const showPerPage = 5
+  let page = 1
   const questions = []
   let sort = 'activity'
 
   console.log(chalk.yellow(`req.query.tab = ${req.query.tab}`))
+  console.log(chalk.yellow(`req.query.filter = ${req.query.filter}`))
 
   try {
     const request = await connectDB()
@@ -23,21 +27,57 @@ export const index = async (req, res) => {
     if (req.query.tab)
       sort = req.query.tab
 
-    /*
-    const response = await axios.get(`${appConfig.API_URL}/2.2/questions?order=desc&sort=${sort}&pagesize=3&tagged=javascript&site=stackoverflow&filter=!4*Zo7ZBUu1hHnaNDR`)
-    const resQuestions = response.data.items
-    */
+    // filter easy, normal or hard
+    if (req.query.filter) {
+      let isLoopQuery = true
 
-    for (let question of resQuestions.items) {
-      let q = question
-      q.title = decodeHtmlEntity(_.unescape(q.title))
-      q.body_markdown = formatBodyMarkdown(q.body_markdown)
-      q.difScore = await getDifficultyLevel(question, request)
-      q.difLv = convetDiffLv(q.difScore)
-      q.timeAgo = ta.ago(new Date(q.last_activity_date * 1000))
-      questions.push(q)
+      while (isLoopQuery) {
+        console.log(chalk.red('Query page = ', page))
+        const response = await axios.get(`${appConfig.API_URL}/2.2/questions?order=desc&sort=${sort}&page=${page}&pagesize=${pagesize}&tagged=javascript&site=stackoverflow&filter=!4*Zo7ZBUu1hHnaNDR`)
+        const resQuestions = response.data.items
 
-      console.log(chalk.green(`Difficulty Score = ${q.difScore}`))
+        // no data
+        if (!resQuestions.length) {
+          isLoopQuery = false
+          break
+        }
+
+        // check each question from api
+        for (let question of resQuestions) {
+          let q = await insertAttrQuestion(question, request)
+          console.log(chalk.red('q ' + q.question_id + ' = ' + q.difLv))
+
+          // match level
+          if (req.query.filter.toLowerCase() === q.difLv)
+            // not duplicate
+            if (_.findIndex(questions, {question_id: q.question_id}) === -1) {
+              questions.push(q)
+              if (questions.length === showPerPage) {
+                isLoopQuery = false
+                break
+              }
+            }
+
+          console.log(chalk.green(`Difficulty Score = ${q.difScore}`))
+        }
+
+        console.log(chalk.red('end while with page :', page))
+        page = page + 1
+      }
+    } else {
+      // no filter show question
+      const response = await axios.get(`${appConfig.API_URL}/2.2/questions?order=desc&sort=${sort}&page=${page}&pagesize=${pagesize}&tagged=javascript&site=stackoverflow&filter=!4*Zo7ZBUu1hHnaNDR`)
+      const resQuestions = response.data.items
+
+      for (let question of resQuestions) {
+        let q = await insertAttrQuestion(question, request)
+        questions.push(q)
+
+        if (questions.length === showPerPage)
+          break
+
+        console.log(chalk.green(`Difficulty Score = ${q.difScore}`))
+      }
     }
   } catch (e) {
     console.log(e)
@@ -67,9 +107,20 @@ const formatBodyMarkdown = (body) => {
 
 const convetDiffLv = (score) => {
   if (score > 0.6)
-    return 'HARD'
+    return 'hard'
   else if (score > 0.4)
-    return 'NORMAL'
+    return 'normal'
   else
-    return 'EASY'
+    return 'easy'
+}
+
+const insertAttrQuestion = async (q, request) => {
+  let result = q
+  result.title = decodeHtmlEntity(_.unescape(q.title))
+  result.body_markdown = formatBodyMarkdown(q.body_markdown)
+  result.difScore = await getDifficultyLevel(q, request)
+  result.difLv = convetDiffLv(q.difScore)
+  result.timeAgo = ta.ago(new Date(q.last_activity_date * 1000))
+
+  return Promise.resolve(result)
 }
